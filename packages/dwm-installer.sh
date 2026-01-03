10ae04b484355732b47e4bda36ef0d123601c701
jesus <thejesus567@gmail.com>
2026-01-02 21:19:13 -0600

    updated dwm installer, added dwm blocks

--- packages/dwm-installer.sh
+++ packages/dwm-installer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 repo="https://github.com/thejesus567/dwm.git"
+blocks_repo="https://github.com/UtkarshVerma/dwmblocks-async.git"
 
 if command -v git >/dev/null 2>&1; then
   echo "git is already installed"
@@ -16,6 +17,13 @@ else
   git clone $repo ~/dwm
 fi
 
+if [ -d /dwmblocks-async ]; then
+  echo "dwm blocks is already installed"
+else
+  echo "dwm is not i the home directory"
+  git clone $blocks_repo ~/dwmblocks-async
+fi
+
 if command -v make >/dev/null 2>&1; then
   echo "make is already installed"
 else
@@ -25,4 +33,8 @@ fi
 
 echo "Compiling dwm..."
 cd ~/dwm
+sudo make clean install
+
+mv ~/dwm/config-blocks.h ~/dwmblocks-async/config.h
+cd ~/dwmblocks-async
 sudo make clean install
