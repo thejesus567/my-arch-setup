ef1285ad5e2c1a188b6e01e5f65ff596f335bd15
jesus <thejesus567@gmail.com>
2026-01-06 21:28:40 -0600

    added comments

--- installer-packages.sh
+++ installer-packages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Installing packages..."
 for package in $(cat packages/packages-list); do
   sudo pacman -S --needed --noconfirm $package
 done
 
+echo "Setting up DWM (thejesus567)..."
 ./packages/dwm-installer.sh
