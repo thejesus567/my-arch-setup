8c30ab47a20d2eeb5273f93a602dbcf5d3707ad7
Jesús Iván Lemus Cervantes <61330835+thejesus567@users.noreply.github.com>
2026-01-18 22:50:24 -0600

    Clean up line breaks in installer-packages.sh
    Removed unnecessary line breaks between package installer calls.

--- installer-packages.sh
+++ installer-packages.sh
@@ -2,15 +2,11 @@
 
 # pacman packages
 ./packages/pacman-packages-installer.sh
-
 # AUR
 ./packages/aur-helper-installer.sh
-
 # yay packages
 ./packages/yay-packages-installer.sh
-
 # Build of DWM
 ./packages/dwm-installer.sh
-
 #Symlink vi
 ./packages/vi-installer.sh
